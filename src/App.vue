971f8961d6c99900bd1e6d42ebea68bd774c7b37
zcong1993 <1005997919@qq.com>
2017-04-07 15:04:56 +0800

    layout 📫

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,75 @@
 <template>
-  <mu-appbar title="Title">
-    <mu-icon-button icon="close" slot="left"/>
-    <mu-icon-menu icon="more_vert" slot="right">
-      <mu-menu-item title="菜单 1"/>
-      <mu-menu-item title="菜单 2"/>
-      <mu-menu-item title="菜单 3"/>
-      <mu-menu-item title="菜单 4"/>
-      <mu-menu-item title="菜单 5"/>
-    </mu-icon-menu>
-  </mu-appbar>
+  <div class="app">
+    <mu-appbar title="Title" class="appbar" :class="{'nav-hide': !open}">
+      <mu-icon-button icon="menu" slot="left" @click="toggleNav"/>
+      <mu-icon-button icon="star" slot="right"/>
+      <mu-icon-button icon="star" slot="right"/>
+    </mu-appbar>
+    <app-drawer :open="open" :docked="docked" @close="toggleNav"></app-drawer>
+    <div class="content" :class="{'nav-hide': !open}">
+
+    </div>
+  </div>
 </template>
 
 <script>
+  import AppDrawer from './components/AppDrawer'
   export default {
-    name: 'app'
+    name: 'app',
+    components: {
+      'app-drawer': AppDrawer
+    },
+    data() {
+      const desktop = isDesktop()
+      return {
+        open: desktop,
+        docked: desktop,
+        desktop
+      }
+    },
+    methods: {
+      toggleNav() {
+        this.open = !this.open
+      }
+    }
+  }
+  function isDesktop() {
+    return window.innerWidth > 993
   }
 </script>
+
+<style lang="less">
+  @import "../node_modules/muse-ui/less/vars.less";
+  .appbar{
+    position: fixed;
+    left: 256px;
+    right: 0;
+    top: 0;
+    width: auto;
+    transition: all .45s @easeOutFunction;
+    &.nav-hide {
+      left: 0;
+    }
+  }
+  .content{
+    padding-top: 56px;
+    padding-left: 256px;
+    transition: all .45s @easeOutFunction;
+    &.nav-hide {
+      padding-left: 0;
+    }
+  }
+  @media (min-width: 480px) {
+    .content{
+      padding-top: 64px;
+    }
+  }
+  @media (max-width: 993px) {
+    .appbar {
+      left: 0;
+    }
+    .content{
+      padding-left: 0;
+    }
+  }
+</style>
---
+++ src/components/AppDrawer.vue
@@ -0,0 +1,30 @@
+<template>
+  <mu-drawer :open="open" :docked="docked" @close="handleClose">
+      <mu-list>
+        <mu-list-item title="Menu Item 1"/>
+        <mu-list-item title="Menu Item 2"/>
+        <mu-list-item title="Menu Item 3"/>
+        <mu-list-item title="Close" @click="handleClose"/>
+      </mu-list>
+    </mu-drawer>
+</template>
+
+<script>
+  export default {
+    props: {
+      open: {
+        type: Boolean,
+        default: true
+      },
+      docked: {
+        type: Boolean,
+        default: true
+      }
+    },
+    methods: {
+      handleClose() {
+        this.$emit('close')
+      }
+    }
+  }
+</script>
