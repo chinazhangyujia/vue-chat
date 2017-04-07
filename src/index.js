b6e49a2bb11be48292f1f2804c0fb08b94155c96
zcong1993 <1005997919@qq.com>
2017-04-07 10:11:21 +0800

    use material-design-icons-iconfont lib ♋

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import MuseUI from 'muse-ui'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'muse-ui/dist/muse-ui.css'
 import App from './App'
 
